451ccb345e56dac8bf2fbf933daa9903f91e086b
muuufii <animateoffical@gmai.com>
2021-05-31 05:18:34 -0700

    jwt auth test project

---
+++ express-jwt-auth-test/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+var cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/authRoutes');
+const {requireAuth, checkUser} = require('./middlewere/authMiddle');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json())
+app.use(cookieParser())
+
+// view engine
+app.set('view engine', 'ejs');
+``
+// database connection
+const dbURI = 'mongodb+srv://test123:test123@cluster0.oevfk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes)
---
+++ express-jwt-auth-test/middlewere/authMiddle.js
@@ -0,0 +1,46 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User')
+
+const requireAuth = (req, res, next) => {
+
+    const token = req.cookies.jwt;
+
+    //verif token
+    if (token) {
+        jwt.verify(token, 'asjkdga7d788939ffggshhsh', (err, decodedToken) =>{
+            if(err){
+                console.log(err.messege);
+                res.redirect('/login');
+            }else {
+                console.log(decodedToken);
+                next();
+            }
+        })
+    }else{
+        res.redirect('/login');
+    }
+
+}
+
+
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token){
+        jwt.verify(token, 'asjkdga7d788939ffggshhsh', async (err, decodedToken) =>{
+            if(err){
+                console.log(err.messege);
+                next();
+            }else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }});
+    } else {
+        res.locals.user = null;
+        next()
+    }
+}
+
+
+module.exports = { requireAuth, checkUser };
